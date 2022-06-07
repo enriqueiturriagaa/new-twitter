7cd708f38d457c9dd2f0f8c3c19f1ec64ff447be
enrique.zone <enriqueiturriagaa@gmail.com>
2022-06-07 09:37:29 -0400

    starting the day commit

---
+++ components/Tweet.js
@@ -0,0 +1,10 @@
+import timeago from "lib/timeago";
+
+export default function Tweet({ tweet }) {
+  return (
+    <p>
+      {timeago.format(new Date(tweet.createdAt))} {tweet.author.name}{" "}
+      {tweet.content}
+    </p>
+  );
+}
