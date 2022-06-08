81f0018f309978fe06e18d233d73b01eb03072db
enrique.zone <enriqueiturriagaa@gmail.com>
2022-06-08 15:58:13 -0400

    Last commit  - Jumping to next week - Incomplete Project

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,10 +1,50 @@
+import Image from "next/image";
 import timeago from "lib/timeago";
+import Link from "next/link";
 
-export default function Tweet({ tweet }) {
+const Tweet = ({ tweet }) => {
   return (
-    <p>
-      {timeago.format(new Date(tweet.createdAt))} {tweet.author.name}{" "}
-      {tweet.content}
-    </p>
+    <div className="mb-4">
+      <div className="flex flex-shrink-0 p-4 pb-0">
+        <div className="flex-shrink-0 block group">
+          <div className="flex items-center">
+            <div>
+              {tweet.author.image && (
+                <Image
+                  className="w-64 h-64 rounded-full"
+                  src={tweet.author.image}
+                  alt=""
+                  width="40"
+                  height="40"
+                />
+              )}
+            </div>
+            <div className="ml-3 -mt-6">
+              <p className="">
+                <Link href={`/${tweet.author.name}/status/${tweet.id}`}>
+                  <a>
+                    <span className="text-base font-medium leading-6 color-primary hover:underline">
+                      {tweet.author.name}
+                    </span>
+                  </a>
+                </Link>
+                <span className="pl-1 text-sm font-light leading-5 color-dimmed">
+                  <a className="hover:underline">
+                    {timeago.format(new Date(tweet.createdAt))}
+                  </a>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pl-16 -mt-6">
+        <p className="flex-shrink pl-1 pr-2 text-base font-normal color-primary width-auto">
+          {tweet.content}
+        </p>
+      </div>
+    </div>
   );
-}
+};
+
+export default Tweet;
